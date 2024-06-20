fcc5398dc3d4a2d39fb7bc244d37de60259d704f
EronAlves1996 <eron.alves@rocketmail.com>
2024-06-19 22:23:49 -0300

    chore: swap project for spring

---
+++ src/main/java/com/eronalves/messagingsystem/MessagingsystemApplication.java
@@ -0,0 +1,13 @@
+package com.eronalves.messagingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessagingsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessagingsystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eronalves/messagingsystem/MessagingsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eronalves.messagingsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessagingsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
