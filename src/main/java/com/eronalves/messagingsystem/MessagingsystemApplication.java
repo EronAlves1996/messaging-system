9455ba898003b7a1fc1f20a06d89eeea2b5f57a7
EronAlves1996 <eron.alves@rocketmail.com>
2024-06-19 22:37:56 -0300

    feat: send a message

--- src/main/java/com/eronalves/messagingsystem/MessagingsystemApplication.java
+++ src/main/java/com/eronalves/messagingsystem/MessagingsystemApplication.java
@@ -1,13 +1,36 @@
 package com.eronalves.messagingsystem;
 
+import java.util.Scanner;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.core.KafkaTemplate;
 
 @SpringBootApplication
-public class MessagingsystemApplication {
+public class MessagingsystemApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MessagingsystemApplication.class, args);
-	}
+  private final KafkaTemplate<String, String> kafkaTemplate;
+
+  public MessagingsystemApplication(
+      KafkaTemplate<String, String> kafkaTemplate) {
+    this.kafkaTemplate = kafkaTemplate;
+  }
+
+  public static void main(String[] args) {
+    SpringApplication.run(MessagingsystemApplication.class, args);
+  }
+
+  @Override
+  public void run(String... args) throws Exception {
+    System.out.println("[PROGRAM] Type some text!!");
+    try (var scanner = new Scanner(System.in)) {
+      while (true) {
+        String nextLine = scanner.nextLine();
+        System.out.println("[PRODUCER] %s".formatted(nextLine));
+        this.kafkaTemplate.send("test", nextLine);
+      }
+    }
+  }
 
 }
